4d190c36086bb5b0c068bf538ff4ed6e170a6d1d
Elton Dornelas <eltondornelas@gmail.com>
2020-05-04 11:31:51 -0300

    Adding New Solved Challenges - Finding the Percentage - Text Alignment - Text Wrap

---
+++ hacker_rank.py
@@ -0,0 +1,13 @@
+# Challenge: List Comprehensions -> list_comprehension.py
+
+# Challenge: Find the Runner-Up Score! -> runner_up_score.py
+
+# Challenge: Nested Lists -> nested_lists.py
+
+# Challenge: Finding the percentage -> finding_the_percentage.py
+
+# Challenge: Text Alignment -> text_alignment.py
+
+# Challenge: Text Wrap -> text_wrap.py
+
+# Challenge: Designer Door Mat
--- python_challenges.py
+++
@@ -1,7 +0,0 @@
-# Challenge: List Comprehensions -> list_comprehension.py
-
-# Challenge: Find the Runner-Up Score! -> runner_up_score.py
-
-# Challenge: Nested Lists -> nested_lists.py
-
-
